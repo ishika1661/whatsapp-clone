3d8d8ff713c8af6ca401f5a6505d5ca6ca83bdae
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2020-11-21 11:14:55 +0530

    basic skeleton

---
+++ whatsapp-mern/src/components/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Chat.css";
+
+function Chat() {
+    return (
+        <div className="chat">
+            This is Chat
+        </div>
+    );
+}
+
+export default Chat
---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDCQc-YR8_PQYduDfLI3M8DBunxSikhA_s",
+    authDomain: "whatsapp-mern-98551.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-98551.firebaseio.com",
+    projectId: "whatsapp-mern-98551",
+    storageBucket: "whatsapp-mern-98551.appspot.com",
+    messagingSenderId: "70556114717",
+    appId: "1:70556114717:web:9ec01d9b2e9a503e5acbf4"
+  };
