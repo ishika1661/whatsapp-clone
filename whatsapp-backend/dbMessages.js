2c31935d3953f10ebc30983f03d2eac94bbc7a5c
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2020-11-25 17:22:00 +0530

    backend done

--- whatsapp-backend/dbMessages.js
+++ whatsapp-backend/dbMessages.js
@@ -4,6 +4,8 @@ const whatsappSchema = mongoose.Schema({
     message: String,
     name: String,
     timestamp: String,
+    received: Boolean
 });
 
-export default mongoose.model('messageContent', whatsappSchema);
+export default mongoose.model('messagecontent', whatsappSchema);
+// const URI = "mongodb+srv://admin:vhPmqC05Rchn6QqP@cluster0.snwrx.mongodb.net/db?retryWrites=true&w=majority"
--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -1,29 +1,92 @@
+// importing
 import express from 'express';
 import mongoose from 'mongoose';
-import dbMessages from './dbMessages.js';
-// 6q8rHzRnfN1mht3k
+import Messages from './dbMessages.js';
+import Pusher from 'pusher';
+import cors from 'cors';
+
+// app config
 const app = express();
 const port = process.env.PORT || 9000;
 
-const URI = "mongodb+srv://admin:6q8rHzRnfN1mht3k@cluster0.snwrx.mongodb.net/db?retryWrites=true&w=majority"
-mongoose.connect(URI, {
-    useCreateIndex: true,
+const pusher = new Pusher({
+    appId: "1111232",
+    key: "5619e67fb9fb13caeb5e",
+    secret: "b7083512ec7f32631e9e",
+    cluster: "ap2",
+    useTLS: true
+}); 
+
+// middleware
+app.use(express.json());
+// everything will accepted for testing purpose
+app.use(cors());
+// or can use:  
+    // app.use((req, res, next) => {
+    //    res.setHeader('Access-Control-Allow-Origin','*');
+    //    res.setHeader('Access-Control-Allow-Headers', '*');
+    //    next();
+    // });
+
+// DB config (MongoDb)
+const connection_url = `mongodb+srv://admin:vhPmqC05Rchn6QqP@cluster0.snwrx.mongodb.net/db?retryWrites=true&w=majority`
+mongoose.connect(connection_url, {
+    userCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+    useUnifiedTopology: true,
+});
 
-app.get('/',(req, res) =>res.status(200).send("hello world"));
+const db = mongoose.connection;
 
-app.post('/api/v1/messsage/new', (req, res) => {
-    const dbMessage = req.body;
+db.once('open', () => {
+    console.log('DB is connected');
+    //make sure the collection name matches the one in Database
+    const msgCollection = db.collection ('messagecontents');
+    const changeStream = msgCollection.watch();
+    changeStream.on('change', (change) => {
+        //console.log('change', change);
+        if(change.operationType === 'insert') {
+            const messageDetails = change.fullDocument;
+            pusher.trigger('messages', 'inserted',
+                {
+                    name: messageDetails.name,
+                    message: messageDetails.message,
+                    timestamp: messageDetails.timestamp,
+                    received: messageDetails.received,
+                });
+        } else {
+            console.log('Error trigger Pusher');
+        }
+    });
+});
 
-    dbMessages.create(dbMessage, (err, data) => {
-        if (err) {
+
+// API Routes
+app.get('/',(req, res)=>res.status(200).send('hello world'));
+// status(200) -> okay
+// get back/download all message from server to the client
+app.get('/messages/sync', (req, res) => {
+    Messages.find((err, data) => {
+        if(err) {
             res.status(500).send(err)
         } else {
-            res.status(201).send(`new message created: \n ${data}`);
+            res.status(200).send(data)
         }
     })
 })
+// push all messages to the database/server
+app.post('/messages/new', (req, res) => {
+    const dbMessage = req.body
 
-app.listen(port, ()=>console.log(`Listening on localhost:${port}`));
+    Messages.create(dbMessage, (err, data) => {
+        if(err) {
+            // status(500) -> internal server error
+            res.status(500).send(err)
+        } else {
+            // status(201) -> created okay
+            res.status(201).send(data)
+        }
+    })
+})
+// Listen 
+app.listen(port,() =>console.log(`Listening on localhost:${port}`));    
